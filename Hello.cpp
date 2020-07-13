e0050f2e2071f771115b9a100d35170c3aec8655
ctcdist <ctcdist@gmail.com>
2020-07-13 14:59:19 -0400

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -6,5 +6,7 @@ int main() {
     std::cout << "Hello World - Jason !";
         std::cout << "Hello World - Alex !";
             std::cout << "Hello World - Cynthia !";
+    
+                std::cout << "Hello World - Mike !";
     return 0;
 }
